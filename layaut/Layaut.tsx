ef9ca275f9dfc1f464aac75fc95d94fefca5d0fc
Yunus <ibadovunus55@gmail.com>
2024-09-09 10:46:27 +1300

    adaptiv

--- layaut/Layaut.tsx
+++ layaut/Layaut.tsx
@@ -10,7 +10,7 @@ interface LayautProps {
 const Layaut: React.FC<LayautProps> = ({ children }) => {
     return (
         <>
-            <header className='w-full fixed  bg-bgMain h-[12vh] text-white text-[16px]  flex items-center justify-between pt-[2%] pl-[3%] pr-[3%]'>
+            <header className='w-full fixed  bg-bgMain h-[12vh] text-white text-[16px]  flex items-center justify-between pt-[2%] pl-[5%] pr-[5%]'>
                 <div className="w-[25%]">
                     <Image src="/logo.svg" alt="logo" width={200} height={200} />
                 </div>
@@ -26,7 +26,59 @@ const Layaut: React.FC<LayautProps> = ({ children }) => {
                 </div>
             </header>
             {children}
-            <footer>
+            <footer className= 'pb-[3%] bg-bgMain pl-[5%] pr-[5%]'>
+                <div className="">
+                    <div className="flex items-center mb-[2%] gap-[20%]">
+
+                        <div className="flex justify-center flex-col gap-[15vh]">
+                            <Image className='lg:w-[200px]  xl:w-[250px] ' src="/logo.svg" alt="logo" width={250} height={200} />
+
+                            <div className="flex pr-[15%] justify-between">
+                                <Image src="/Instagram.svg" alt="logo" width={30} height={30} />
+                                <Image src="/Facebook.svg" alt="logo" width={30} height={30} />
+                                <Image src="/Twitter.svg" alt="logo" width={30} height={30} />
+                            </div>
+                        </div>
+
+                        <div className="flex w-[70%] justify-around">
+                            <div className="text-gray-500 flex flex-col gap-[.5vh]">
+                                <h2 className=' lg:text-[23px] xl:text-[30px] font-[600] text-white'>Explore</h2>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">About Us</Link>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">Services</Link>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">Pricing</Link>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">Blog</Link>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">Contact</Link>
+                            </div>
+                            <div className="text-gray-500 flex flex-col gap-[.5vh]">
+                                <h2 className='lg:text-[23px] xl:text-[30px] font-[600] text-white'>Contact</h2>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">Email</Link>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">Phone</Link>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">Address</Link>
+                                <Link className='lg:text-[16px] xl:text-[20px]' href="/about">Media</Link>
+                            </div>
+                            <div className="text-gray-500 flex flex-col gap-[.5vh]">
+                                <h2 className='lg:text-[23px] xl:text-[30px] font-[600] text-white'>Newsletter</h2>
+                                <p className='lg:text-[16px] xl:text-[20px] mb-[3%]' >Subscribe to our newsletter to stay informed about <br /> our latest products, services, and promotions. <br />
+                                    Feel free to unsubscribe anytime!</p>
+
+                                <div className="flex relative justify-between border rounded border-gray-500 lg:pt-[1.5%] lg:pl-[4%] lg:pr-[4%] lg:pb-[1.5%] xl:pl-[6%] xl:pr-[6%] xl:pb-[3%]">
+                                    <input className='bg-bgMain outline-none lg:text-[16px] xl:text-[20px] type="email'
+                                        placeholder='Enter Your Email Adress...'
+                                    />
+
+                                    <Image src="/Arrow_Icon.svg" alt="arrow" width={20} height={20} />
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <div className="border-t pt-[.5%] flex justify-between text-gray-500 border-gray-500">
+                        <p className='lg:text-[14px] xl:text-[16px]'>Copyright © 2023 for WaveNet. All rights reserved.</p>
+                        <p className='lg:text-[14px] xl:text-[16px]'>Terms & Condition  |  Privacy Policy</p>
+                    </div>
+                </div>
             </footer>
         </>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,54 +19,107 @@ export default function Home() {
       <Layaut>
         {/* <Image className="absolute  top-0 right-0 w-full z-[1]"  src="/first_bg.svg" alt="backround" width={1500} height={1000}/>
  */}        <div className="pt-[15%] bg-bgMain ">
-          <h1 className="text-[160px] z-[2] text-white leading-[22vh] text-center">
+          <h1 className=" lg:text-[100px] xl:text-[160px] z-[2] text-white leading-[22vh] text-center">
             YOUR BEST <br /> <span className="italic">IT </span>
             PARTNERS
           </h1>
-          <p className="z-[2] text-center text-white text-[16px] mb-[30px]">From custom software development to cybersecurity, our team of experts is dedicated <br />
+          <p className="z-[2] text-center text-white lg:text-[14px] xl:text-[16px] mb-[30px]">From custom software development to cybersecurity, our team of experts is dedicated <br />
             to delivering solutions that are tailored to your unique needs.</p>
 
-          <button className="flex items-center mb-[10%] gap-[10%] border pt-[10px] pb-[10px] mx-auto pr-[40px] pl-[20px] rounded-[5px] text-white whitespace-nowrap">EXPLORE NOW <Image src="/arrow.svg" alt="стрелка" width={50} height={50} /></button>
+          <button className="flex lg:text-[14px] xl:text-[16px] items-center mb-[10%] gap-[10%] border pt-[10px] pb-[10px] mx-auto pr-[40px] pl-[20px] rounded-[5px] text-white whitespace-nowrap">EXPLORE NOW <Image src="/arrow.svg" alt="стрелка" width={50} height={50} /></button>
 
-          <h1 className="text-white mb-[4%] text-[70px] leading-[80px] pl-[5%]">Why <br /> Choose Us?</h1>
+          <h1 className="text-white mb-[4%] lg:text-[50px] xl:text-[70px] leading-[80px] pl-[5%]">Why <br /> Choose Us?</h1>
           <div className="pl-[5%] flex w-full pr-[5%] justify-between">
             <div className="flex flex-col gap-[2vh]">
-              <Image className="mb-[2%] h-[15vh]" src="/Vector.svg" alt="texnology" width={150} height={150} />
-              <h2 className="text-[28px] text-white weight-[600]">expertise</h2>
-              <p className="text-[16px] text-white">Our team of experienced experts <br /> have the knowledge and expertise <br />
+              <Image className="mb-[2%] lg:h-[12vh] xl:h-[15vh]" src="/Vector.svg" alt="texnology" width={150} height={150} />
+              <h2 className="lg:text-[22px] xl:text-[28px] text-white weight-[600]">expertise</h2>
+              <p className="lg:text-[14px] xl:text-[16px] text-white">Our team of experienced experts <br /> have the knowledge and expertise <br />
                 to deliver innovative IT solutions <br />
                 that meet your unique needs.</p>
             </div>
 
             <div className="flex flex-col gap-[2vh]">
-              <Image className="mb-[2%] h-[15vh]" src="/texnology.svg" alt="texnology" width={150} height={150} />
-              <h2 className="text-[28px]  text-white weight-[600]">technology</h2>
-              <p className="text-[16px]  text-white">We stay up to date with the latest <br /> trends and technologies in the IT <br /> industry, so you can get the most <br /> advanced solutions available.</p>
+              <Image className="mb-[2%] lg:h-[12vh] xl:h-[15vh]" src="/texnology.svg" alt="texnology" width={150} height={150} />
+              <h2 className="lg:text-[22px] xl:text-[28px]  text-white weight-[600]">technology</h2>
+              <p className="lg:text-[14px] xl:text-[16px]  text-white">We stay up to date with the latest <br /> trends and technologies in the IT <br /> industry, so you can get the most <br /> advanced solutions available.</p>
             </div>
 
             <div className="flex flex-col gap-[2vh]">
-              <Image className="mb-[2%] h-[15vh]" src="/solutions.svg" alt="texnology" width={150} height={150} />
-              <h2 className="text-[28px] text-white weight-[600]">solutions</h2>
-              <p className="text-[16px] text-white">We take a personalized approach <br />
+              <Image className="mb-[2%] lg:h-[12vh] xl:h-[15vh]" src="/solutions.svg" alt="texnology" width={150} height={150} />
+              <h2 className="lg:text-[22px] xl:text-[28px] text-white weight-[600]">solutions</h2>
+              <p className="lg:text-[14px] xl:text-[16px] text-white">We take a personalized approach <br />
                 to every project, working closely <br /> with you to understand your <br /> business and create solutions.</p>
             </div>
 
             <div className="flex flex-col mb-[10%] gap-[2vh]">
-              <Image className="mb-[2%] h-[15vh]" src="/results.svg" alt="texnology" width={150} height={150} />
-              <h2 className="text-[28px] text-white weight-[600]">results</h2>
-              <p className="text-[16px] text-white">Our track record speaks for itself – <br /> we've helped businesses of all <br /> sizes and industries achieve their <br /> goals with our IT solutions.</p>
+              <Image className="mb-[2%] lg:h-[12vh] xl:h-[15vh]" src="/results.svg" alt="texnology" width={150} height={150} />
+              <h2 className="lg:text-[22px] xl:text-[28px] text-white weight-[600]">results</h2>
+              <p className="lg:text-[14px] xl:text-[16px] text-white">Our track record speaks for itself – <br /> we've helped businesses of all <br /> sizes and industries achieve their <br /> goals with our IT solutions.</p>
             </div>
 
 
           </div>
         </div>
-        <div className="bg-[url(/bg_sec.svg)] pt-[20%] bg-cover bg-center h-full w-full">
-          <h1 className="font-[600] mb-[1%] text-[90px] text-center">We believe that
+        <div className="bg-[url(/bg_sec.svg)] pb-[10%] lg:pt-[10%] xl:pt-[20%] bg-cover bg-center h-full w-full">
+          <h1 className="font-[600] mb-[1%] lg:text-[70px] xl:text-[90px] text-center">We believe that
             <span className="italic font-[400]"> technology </span> <br />
             can  <span className="italic font-[400]">change</span> the world.
           </h1>
 
-          <p className="text-[18px] text-center">That's why we're committed to delivering innovative IT solutions to businesses of all <br /> sizes. Our team of experienced professionals is dedicated to helping you achieve <br /> your goals and thrive in a rapidly evolving digital landscape.</p>
+          <p className="lg:text-[16px] xl:text-[18px] mb-[8%] text-center">That's why we're committed to delivering innovative IT solutions to businesses of all <br /> sizes. Our team of experienced professionals is dedicated to helping you achieve <br /> your goals and thrive in a rapidly evolving digital landscape.</p>
+
+          <div className="w-[80%] flex mx-auto items-center">
+            <div className="w-[15%]">
+              <h2 className=" text-center lg:text-[35px] xl:text-[45px]">500+</h2>
+              <p className="text-center lg:text-[14px] xl:text-[16px]">Successful Projects</p>
+            </div>
+            <div className="w-[28%] h-[3px] rounded-[5px]  bg-black"></div>
+            <div className="w-[15%]">
+              <h2 className=" text-center lg:text-[35px] xl:text-[45px]">98%</h2>
+              <p className="text-center lg:text-[14px] xl:text-[16px]">Satisfied Clients</p>
+            </div>
+            <div className="w-[28%] h-[3px] rounded-[5px]  bg-black"></div>
+            <div className="w-[15%]">
+              <h2 className="text-center lg:text-[35px] xl:text-[45px]">35+</h2>
+              <p className="text-center lg:text-[14px] xl:text-[16px]">Handled Countries</p>
+            </div>
+          </div>
+
+        </div>
+        <div className="bg-bgMain pt-[10%] pb-[10%] pl-[5%] pr-[5%]">
+          <div className="flex justify-between ">
+            <h1 className="lg:text-[55px] xl:text-[80px] mb-[5%] text-white font-[600]">Client Stories</h1>
+            <Image className="lg:w-[30px] lg:h-[100px] xl:w-[50px] xl:h-[50px] " src="/Arrow_Icon.svg" alt="arrow" width={50} height={50} />
+          </div>
+
+          <div className="flex mb-[10%]">
+            <div className="border-r w-1/2 pr-[5%] text-white">
+              <h2 className="lg:text-[35px] xl:text-[50px] text-green-100 mb-[1%] font-[600]">Amazing!</h2>
+              <p className=" lg:text-[15px] xl:text-[20px]  mb-[5%]">“WaveNet's cybersecurity solution gave us the peace of mind we <br /> needed to focus on our business. They took the time to understand <br /> our unique needs and created a solution that protected our sensitive <br /> data and ensured compliance with industry regulations.”</p>
+              <div className="flex pr-[10%] gap-[2%] justify-end">
+                <h2 className="text-end"><span className="font-[600]">Tom Johnson</span> <br />
+                  CIO of Mika Medika Healthcare
+                </h2>
+
+                <Image src="/man.svg" alt="man" width={50} height={50} />
+              </div>
+            </div>
+
+            <div className=" w-1/2 pl-[5%] text-white">
+              <h2 className=" lg:text-[35px] xl:text-[50px] text-green-100 mb-[1%] font-[600]">Best Service!</h2>
+              <p className="lg:text-[15px] xl:text-[20px]  lg:mb-[10%] xl:mb-[5%]">“We were struggling to keep up with the demands of our growing <br /> business until we partnered with WaveNet. Their custom software <br /> development solution has helped us streamline our operations and <br /> improve efficiency, saving us time and money.”</p>
+              <div className="flex pr-[10%] gap-[2%] justify-end">
+                <h2 className="text-end"><span className="font-[600]">Jane Doe</span> <br />
+                  COO of DEF Manufacturing                </h2>
+
+                <Image src="/women.svg" alt="man" width={50} height={50} />
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full flex justify-center">
+            <Image src="/brands.svg" alt="brands" width={1200} height={100} />
+          </div>
         </div>
 
       </Layaut>
